7515ba69f198005e3fa3a8bdae328b1b8914aa65
Agustina De Adamo <agustina.adamo@valtech.com>
2021-01-01 16:33:58 -0300

    Initial configuration

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <div>hello word</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+export default HelloWorld;
