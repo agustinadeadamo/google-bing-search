652d0125fcca98fab7d085aa4cdb611080660ea4
Agustina De Adamo <agustina.adamo@valtech.com>
2021-01-01 17:08:27 -0300

    Modified gitignore file and added gh pages configuration

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+    publicPath: "/search-app/",
   },
   entry: "./src/index.js",
   resolve: {
